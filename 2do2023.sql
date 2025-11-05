4d19d719bf8cdbed744b301af58d59b769dfb4ab
Ariel Buhid <arielbuhid@hotmail.com>
2025-11-05 11:27:38 -0300

    creé el procedimiento en 2do2023.sql

--- 2do2023.sql
+++ 2do2023.sql
@@ -12,3 +12,19 @@ BEGIN TRANSACTION;
 		ERROR_MESSAGE() AS ErrorMessage
 		ROLLBACK TRANSACTION
 	END CATCH
+
+declare @ProvinciaID as int = 1
+declare @Fecha as date = null
+go
+create procedure getdatte3(@ProvinciaID int, @Fecha date = null) as 
+begin
+
+SELECT C.ClinicaDesc, D.Fecha, C.ProvinciaID
+	FROM  CLINICAS C
+	JOIN DISPENSAS D ON D.ClinicaID=C.ClinicaID
+	WHERE C.ProvinciaID = @ProvinciaID AND (@Fecha IS NULL OR D.Fecha = @Fecha)
+
+end
+
+exec getdatte3 @ProvinciaID=1, @Fecha = '09/01/2023'
+
